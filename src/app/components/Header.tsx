bb65fc2e54759b746f143274a530fd6466a14869
Yegan Salehi <salehisediqe86@gmail.com>
2025-11-17 10:12:39 -0800

    Created Navbar by hard coded in Next.js by RadixUI with Tailwindcss, next level is making the navbar dynamic, should come from strapi.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,14 +3,15 @@ import { TabNav } from "@radix-ui/themes";
 
 export default function Header(){
     return(
-        <div>
-            <TabNav.Root>
-	<TabNav.Link href="#">
-		Account
-	</TabNav.Link>
+        <div className="flex w-full bg-slate-200 p-3 fixed top-0 left-0 z-50">
+
+        <TabNav.Root>
+
+	<TabNav.Link href="#">Account</TabNav.Link>
 	<TabNav.Link href="#">Documents</TabNav.Link>
 	<TabNav.Link href="#">Settings</TabNav.Link>
-</TabNav.Root>
+     
+     </TabNav.Root>
 
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ import Header from "./components/Header";
 export default function Home() {
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-         <div className="flex min-h-screen p-1  m-2 bg-slate-200">
+         
          <Header/>
-         </div>
-         <div>
+        
+         <div className="pt-20 px-4">
          <Flex direction="column" gap="2">
           <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+              Hello world!
+          </h1>
 			<Text>Hello from Radix Themes :)</Text>
 			<Button>Let's go</Button>
 		</Flex>
